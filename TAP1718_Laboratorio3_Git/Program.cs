db13c3bc86cb60af8aeb88dd05bd4e96bbbb542e
Antares1996 <56757083+Antares1996@users.noreply.github.com>
2019-10-20 16:32:20 +0200

    Update Program.cs

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -9,7 +9,7 @@ namespace TAP1718_Laboratorio3_Git
             Console.WriteLine("Se leggete questo messaggio, significa che siete tornati indietro nel tempo!");
                         Console.WriteLine("ciao!");
             Console.WriteLine("mondo!");
-            Console.WriteLine("tutto bene!");
+            Console.WriteLine("se leggi questo messaggio hai effettuato il pull correttamente!");
 
         }
     }
